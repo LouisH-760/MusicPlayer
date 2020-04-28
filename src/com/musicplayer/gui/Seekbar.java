11da51a3519359f4de2598638c4513749cb6679a
Louis Hermier <louis.hermier1@etu.univ-lorraine.fr>
2020-04-28 13:19:46 +0200

    Add the seekbar to the UI
    Seekbar is not functional for now

---
+++ src/com/musicplayer/gui/Seekbar.java
@@ -0,0 +1,21 @@
+package com.musicplayer.gui;
+
+import javax.swing.JSlider;
+
+public class Seekbar extends JSlider{
+	public final int MAX = 100000;
+	public final int MIN = 0;
+	
+	public Seekbar() {
+		setMinimum(MIN);
+		setMaximum(MAX);
+		setValue(MIN);
+		setPaintTicks(false);
+		setPaintLabels(false);
+	}
+	
+	public void setPosition(float position) {
+		int redonePosition = (int)(position * 10000);
+		setValue(redonePosition);
+	}
+}
