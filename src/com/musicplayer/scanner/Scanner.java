982b5d1fb27ea7e7b414b47ea1ce43bc6f55def2
Louis Hermier <louis.hermier1@etu.univ-lorraine.fr>
2020-05-01 16:00:36 +0200

    Add javadoc comments to the Scanner interface

--- src/com/musicplayer/scanner/Scanner.java
+++ src/com/musicplayer/scanner/Scanner.java
@@ -8,13 +8,25 @@ import java.util.List;
  * See folderScanner for an example of implementation
  */
 public interface Scanner {
-	// get the cover art to display
+	/**
+	 *  get the cover art to display
+	 * @return a list of the images that match the "cover art" criteria (ie named Cover.ext, ...)
+	 */
 	public List<String> getCoverArt();
-	// get all the music files to play
+	/**
+	 *  get all the music files to play
+	 * @return list of all the music files
+	 */
 	public List<String> getAllMusicFiles();
-	// get all the images associated with the content
+	/**
+	 *  get all the images associated with the content
+	 * @return list of all the images
+	 */
 	public List<String> getAllImageFiles();
-	// get the name of the album that will be played
-	// (is ideally then replaced by the album gotten from the player)
+	/**
+	 *  get the name of the album that will be played
+	 *  (is ideally then replaced by the album gotten from the player)
+	 * @return : name of the album (can be inferred from the folder name, ...)
+	 */
 	public String getAlbumName();
 }
