c2a96918ae3828624df0e8bbd116e64378a27f3c
Louis Hermier <louis.hermier1@etu.univ-lorraine.fr>
2020-05-12 17:29:45 +0200

    Add skeleton for single album scanner using the Scanner interface
    The goal is to get feature parity with the existing scanner using the new media types, then implement logic using it to make it easier to implement multiple-album playback in the future

---
+++ src/com/musicplayer/scanner/SingleFolderScanner.java
@@ -0,0 +1,22 @@
+package com.musicplayer.scanner;
+
+import java.util.List;
+
+import com.musicplayer.media.Album;
+import com.musicplayer.media.Song;
+
+public class SingleFolderScanner implements Scanner {
+
+	@Override
+	public List<Album> getAlbums() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Song> getSongs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
